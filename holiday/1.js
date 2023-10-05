438fa9fde8a424182a3f727fdd96a85afac42009
kirankishore12 <111950174+kirankishore12@users.noreply.github.com>
2023-10-05 19:14:51 +0530

    Add files via upload

---
+++ holiday/1.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Example: Display an alert when the "Book Now" button is clicked
+    const bookNowButton = document.querySelector(".cta-button");
+    bookNowButton.addEventListener("click", function () {
+        alert("Thank you for choosing Holiday Getaways! We'll contact you shortly.");
+    });
+});
